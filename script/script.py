d338c714b9b71d0ce79a4835875c1b30dd739e5a
M1kayel <139972239+M1kayel@users.noreply.github.com>
2023-08-23 14:52:32 +0400

    script.py

--- script/script.py
+++ script/script.py
@@ -2,11 +2,11 @@
 
 import os                       
 def read_files_in_directory(input_file, output_file):
-    with open(output_file,'w', encoding="utf8") as file_out:
+    with open(output_file,'w') as file_out:
         for i in os.listdir(input_file):
             if i.endswith(".txt"):
                 file_path = os.path.join(input_file,i)
-                with open (file_path,'r', encoding="utf8") as file_in:
+                with open (file_path,'r') as file_in:
                     file_out.write(file_in.read())
 
 INPUT_FILE ="/home/mika/Desktop/test"
