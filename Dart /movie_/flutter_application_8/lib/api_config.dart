86a7cc3b3a6fb9ba312be16c356ecc7d48d96c66
Mika <dfuk4@mail.ru>
2024-08-04 19:26:02 +0400

    my instagram

--- Dart /movie_/flutter_application_8/lib/api_config.dart
+++
@@ -1,6 +0,0 @@
-
-class ApiConfig {
-  static const String apiKey = '2d668ba04cd02af73832511a7dd6bd06';
-  static const String host = 'https://api.themoviedb.org/3';
-  static const String imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
-}
